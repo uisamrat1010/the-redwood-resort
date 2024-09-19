0f2fe9b4ecd17b13e35ab387feee8b6e8543d1a0
uisamrat1010 <uisamratc@gmail.com>
2024-09-19 19:01:07 +0530

    section-29-auth-3

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,4 +1,4 @@
-import supabase, { supabaseUrl } from "./Supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
 export async function getCabins() {
   const { data, error } = await supabase.from("cabins").select("*");
